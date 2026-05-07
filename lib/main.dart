026a8a9b83f41554c4dbf6409b48ce6e96ad9e27
Jack Veech <jackveech06@gmail.com>
2026-05-06 22:22:43 -0400

    Major Landing Refinements
    - Updated main.dart behav. to reflect a more accurate login flow
    - Added homepage for logged in users
    Refined main.dart layout and switch statement

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
-// ignore: unused_import
 import 'package:brigid/firebase_options.dart';
 import 'package:brigid/views/login_view.dart';
 import 'package:brigid/views/register_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:brigid/views/home_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp( MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 232, 6, 112)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 232, 6, 112),
+        ),
       ),
       home: const Routepage(),
-    ),);//changed @8:11 to make usre the app doesnt rebuild everytime
+    ),
+  ); //changed @8:11 to make usre the app doesnt rebuild everytime
 }
 
 class Routepage extends StatelessWidget {
@@ -24,30 +26,28 @@ class Routepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold (
-      appBar: AppBar(title: const Text('Route Page'),),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-              options: DefaultFirebaseOptions.currentPlatform,
-             ),
-        builder: (context, snapshot) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options:DefaultFirebaseOptions.currentPlatform
+    ),
+      builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
             final user = FirebaseAuth.instance.currentUser;
-            if (user?.emailVerified ?? false){
-              print ('user is verified');
+            if (user != null) {
+              if (user?.emailVerified ?? false) {
+                return const HomeView();
+              }
+            } else {
+              return const LoginView();
             }
-            else{
-              print ('user is not verified, please verify your email first');
-            }
-             return const Text('Done');          
           default:
-           return const Text('Loading...');
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
         }
-
-        },  
-      )
-    ,);
+        return const SizedBox();
+      },
+    );
   }
 }
-
