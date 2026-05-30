7a844d977ce02aa97da4c2167013412e17c2e125
jackveech <jackveech06@gmail.com>
2026-05-30 18:11:46 -0400

    Update Files
    Files Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
+<<<<<<< Updated upstream
+=======
+// ignore: unused_import
+import 'package:flutter/material.dart';
+>>>>>>> Stashed changes
 import 'package:brigid/firebase_options.dart';
 import 'package:brigid/views/login_view.dart';
-import 'package:brigid/views/register_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:brigid/views/home_view.dart';
+=======
+>>>>>>> Stashed changes
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
+<<<<<<< Updated upstream
       title: 'SpectraSense Alaytics',
+=======
+      title: 'Flutter Demo',
+>>>>>>> Stashed changes
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 232, 6, 112),
@@ -26,6 +37,7 @@ class Routepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return FutureBuilder(
       future: Firebase.initializeApp(
         options:DefaultFirebaseOptions.currentPlatform
@@ -48,6 +60,29 @@ class Routepage extends StatelessWidget {
         }
         return const SizedBox();
       },
+=======
+    return Scaffold(
+      appBar: AppBar(title: const Text('Route Page')),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('user is verified');
+              } else {
+                print('user is not verified, please verify your email first');
+              }
+              return const Text('Hai!! :3');
+            default:
+              return const Text('Loading...');
+          }
+        },
+      ),
+>>>>>>> Stashed changes
     );
   }
 }
